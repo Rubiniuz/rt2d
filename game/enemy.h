b840dce4af1b98dcfaec7df56b83b8fbe941ffc0
Rubiniuz <ruben.engelage@gmail.com>
2020-01-14 13:14:26 +0100

    remove old code

--- game/enemy.h
+++ game/enemy.h
@@ -3,12 +3,9 @@
 
 #include <rt2d/entity.h>
 #include <rt2d/input.h>
-#include <rt2d/canvas.h>
-#include <rt2d/sprite.h>
 #include <string>
 #include <iostream>
 
-#include "scenes/field.h"
 #include "scenemanager.h"
 
 class Enemy : public Entity
@@ -22,27 +19,9 @@ public:
 
   void UseSprite(std::string toGet);
 
-  void TGA2PixelSprite();
-  void TGA2Grid(std::string tiles, std::string tga);
-  void Master2Grid(std::vector<int> removedparts);
-
-  void DestroyPart(int partnumber);
-
-  char enemySprite;
-
-  PixelSprite mainsprite;
-
-  Canvas* can;
-
-  Field* field;
-
-  std::vector<int> destroyedparts;
+  DynamicSprite* mainsprite;
 
 private:
-
-  Sprite* heightmapsprite;
-
-  std::vector<char> cv;
 
   int gridwidth;
   int gridheight;
