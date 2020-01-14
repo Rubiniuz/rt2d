dc05c0b57c5779defc7f5b2cf5d375cd0b3ec7d5
Rubiniuz <ruben.engelage@gmail.com>
2020-01-14 13:09:41 +0100

    add hover over check in button

--- game/ui/button.cpp
+++ game/ui/button.cpp
@@ -14,12 +14,23 @@ Button::~Button()
 
 void Button::update(float deltaTime)
 {
-
+  Hover();
 }
 
 void Button::UseSprite(string toGet)
 {
   this->addSprite(toGet);
+}
+
+void Button::Hover()
+{
+  int xpos = Singleton<Input>::instance()->getMouseX();
+  int ypos = Singleton<Input>::instance()->getMouseY();
+  if (ypos >= (this->position.y - ((this->sprite()->height() * this->scale.y) / 2)) && ypos <= (this->position.y + ((this->sprite()->height() * this->scale.y) / 2)) && xpos >= (this->position.x - ((this->sprite()->width() * this->scale.x) / 2)) && xpos <= (this->position.x + ((this->sprite()->width() * this->scale.x) / 2)))
+  {
+    this->sprite()->color = RGBAColor(255,255,255,204);
+  }
+  else{ this->sprite()->color = RGBAColor(255,255,255,255); }
 }
 
 bool Button::CheckPressed()
--- game/ui/button.h
+++ game/ui/button.h
@@ -19,6 +19,8 @@ public:
 
   bool CheckPressed();
 
+  void Hover();
+
 private:
 
 };
